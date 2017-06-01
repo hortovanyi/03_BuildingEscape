fd089a15684da7ae1b7306db8262991e5f57b647
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2017-06-01 13:45:50 +1000

    updated position report on two objects

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -19,8 +19,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+    FString ObjectName = GetOwner()->GetName();
+    
+    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s!"), *ObjectName);
 }
 
 
